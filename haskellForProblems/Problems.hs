e68037532e30774081e56296dbc18bdd7dcce1ee
lcb42 <blatherwick.lucy@gmail.com>
2018-03-08 15:53:04 +0000

    updated problems.hs

--- haskellForProblems/Problems.hs
+++ haskellForProblems/Problems.hs
@@ -1,9 +1,15 @@
 
 type Order = Int
 
+--
+type Where = ( _ !! Int )
+
 -- Join two tables
-conjoin :: [[String]] -> [[String]] -> [[String]]
-conjoin t1 t2 = [a ++ b | a <- t1, b <- t2]
+-- With the possibility of including 'Wheres' -> conditions which are used to filter the results.
+-- Is there a way to pass in a list of wheres to be unpacked and applied as filters as explained above.
+--conjoin :: [[String]] -> [[String]] -> [[String]]
+conjoin t1 t2 [] = [a ++ b | a <- t1, b <- t2]
+conjoin t1 t2 wheres = [a ++ b | a <- t1, b <- t2, wheres]
 
 -- Take one row, rearrange to desired order
 orderOne :: [Order] -> [String] -> [String]
