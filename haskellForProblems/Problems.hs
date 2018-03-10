e86e9f125545605d41bf71f99b895514dfe3dae2
xelagee <36332181+xelagee@users.noreply.github.com>
2018-03-10 22:09:02 +0000

    Allowed csv reading

--- haskellForProblems/Problems.hs
+++ haskellForProblems/Problems.hs
@@ -1,3 +1,4 @@
+import Data.List
 
 type Order = Int
 type Where = [Int]
@@ -24,4 +25,25 @@ selectAll ord xs = [selectOne ord x | x <- xs ]
 
 -- Pass in a desired order and two lists of strings to be joined and ordered
 selectFrom :: [Order] -> [[String]] -> [[String]] -> [Where] -> [[String]]
-selectFrom ord xs ys wheres = selectAll ord (conjoin xs ys wheres)
+selectFrom ord xs ys wheres = sort (selectAll ord (conjoin xs ys wheres))
+
+readCsv :: String -> IO [[String]]
+readCsv file = do contents <- readFile file
+                  let lineSep = lines contents
+                  let lists = splitCommaList lineSep
+                  return lists 
+
+splitCommaList :: [String] -> [[String]]
+splitCommaList strings = [splitCommaOnce s | s <- strings]
+
+splitCommaOnce :: String -> [String]
+splitCommaOnce string = case dropWhile (==',') string of "" -> []
+                                                         string' -> w : splitCommaOnce string''
+                                                          where (w, string'') = break (==',') string'
+
+-- Main
+main = do
+       readFirst <- readCsv "1a2.csv"
+       readSecond <- readCsv "1b2.csv"
+       let result = conjoin readFirst readSecond [] 
+       print result
