a340c3d98a71ccaecd90f6fdb4448ebcfe1ae4ba
xelagee <36332181+xelagee@users.noreply.github.com>
2018-03-11 16:54:51 +0000

    Updated with main

--- haskellForProblems/Problems.hs
+++ haskellForProblems/Problems.hs
@@ -1,19 +1,25 @@
+module Main where
+import Tokens
+--import Grammar
 import Data.List
 
 type Order = Int
-type Where = [Int]
+--type Where = [Int]
+type Where = (Int, Int)
 
 -- TODO:Convert from AST strings to items which can be passed into the below functions
 
 -- Join two tables and apply 'wheres if there are any'
-conjoin :: [[String]] -> [[String]] -> [Where] -> [[String]]
-conjoin t1 t2 [] = [a ++ b | a <- t1, b <- t2]
-conjoin t1 t2 wheres = [a ++ b | a <- t1, b <- t2, process a b wheres]
+conjoin :: [[String]] -> [[String]] -> [[String]]
+conjoin t1 t2 = [a ++ b | a <- t1, b <- t2]
 
--- Processes the 'wheres'
-process :: [String] -> [String] -> [Where] -> Bool
-process a b [] = True
-process a b (w:ws) = (a !! (w !! 0) == b !! (w !! 1)) && (process a b ws)
+-- Apply wheres after conjoin
+applyWhere :: [[String]] -> [Where] -> [[String]]
+applyWhere table wheres = [row | row <- table, rowBelongs row wheres]
+
+rowBelongs :: [String] -> [Where] -> Bool
+rowBelongs row [] = True
+rowBelongs row (w:wheres) = (row !! (fst w) == row !! (snd w)) && rowBelongs row wheres
 
 -- Take one row, rearrange to desired order
 selectOne :: [Order] -> [String] -> [String]
@@ -23,9 +29,20 @@ selectOne ord xs = [ xs !! o | o <- ord]
 selectAll :: [Order] -> [[String]] -> [[String]]
 selectAll ord xs = [selectOne ord x | x <- xs ]
 
+-- Take a list of lists and return each list as a string on a newline
+formatOutput :: [[String]] -> String
+formatOutput [] = []
+formatOutput (s:[]) = mergeList s
+formatOutput (s:strings) = mergeList s ++ "\n" ++ formatOutput strings
+
+mergeList :: [String] -> String
+mergeList [] = []
+mergeList (s:[]) = s
+mergeList (s:string) = s ++ "," ++ mergeList string
+
 -- Pass in a desired order and two lists of strings to be joined and ordered
-selectFrom :: [Order] -> [[String]] -> [[String]] -> [Where] -> [[String]]
-selectFrom ord xs ys wheres = sort (selectAll ord (conjoin xs ys wheres))
+selectFrom :: [Order] -> [[String]] -> [[String]] -> [[String]]
+selectFrom ord xs ys = sort (selectAll ord (conjoin xs ys))
 
 readCsv :: String -> IO [[String]]
 readCsv file = do contents <- readFile file
@@ -42,8 +59,18 @@ splitCommaOnce string = case dropWhile (==',') string of "" -> []
                                                           where (w, string'') = break (==',') string'
 
 -- Main
+--main = do
+--       readFirst <- readCsv "5a3.csv"
+--       readSecond <- readCsv "5b3.csv"
+--       let result = conjoin readFirst readSecond
+--       let filtered = applyWhere result [(1,2)]
+--       let final = sort $ selectAll [0,3] filtered       
+--       print $ formatOutput final
+
 main = do
-       readFirst <- readCsv "1a2.csv"
-       readSecond <- readCsv "1b2.csv"
-       let result = conjoin readFirst readSecond [] 
-       print result
+ s <- readFile "text.txt" 
+ let tokens = alexScanTokens s
+ print tokens
+
+
+
