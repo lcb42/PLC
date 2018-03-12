21186061fb82b3b212caf6a08acd966dce740e7d
xelagee <36332181+xelagee@users.noreply.github.com>
2018-03-12 17:41:18 +0000

    Almost done????

--- haskellForProblems/Problems.hs
+++ haskellForProblems/Problems.hs
@@ -3,15 +3,21 @@ import Tokens
 import Grammar
 import Data.List
 
-type Order = Int
-type WherePair = (Int, Int)
+type Order = String
+type WherePair = (String, String)
 type VarMap = (String, Int)
-
--- TODO:Convert from AST strings to items which can be passed into the below functions
 
 -- Join two tables and apply 'wheres if there are any'
 conjoin :: [[String]] -> [[String]] -> [[String]]
 conjoin t1 t2 = [a ++ b | a <- t1, b <- t2]
+
+createWherePairs :: Exp -> [WherePair]
+createWherePairs (TakeFromWhere x y z) = getPairs z
+createWherePairs (TakeFrom x y) = []
+
+getPairs :: Where -> [WherePair]
+getPairs (Eq x) = [x]
+getPairs (And x y) = getPairs x ++ getPairs y
 
 -- Apply wheres after conjoin
 applyWhere :: [[String]] -> [WherePair] -> [[String]]
@@ -19,15 +25,19 @@ applyWhere table wheres = [row | row <- table, rowBelongs row wheres]
 
 rowBelongs :: [String] -> [WherePair] -> Bool
 rowBelongs row [] = True
-rowBelongs row (w:wheres) = (row !! (fst w) == row !! (snd w)) && rowBelongs row wheres
+rowBelongs row (w:wheres) = (row !! (read (fst w)) == row !! (read (snd w))) && rowBelongs row wheres
 
 -- Take one row, rearrange to desired order
 selectOne :: [Order] -> [String] -> [String]
-selectOne ord xs = [ xs !! o | o <- ord]
+selectOne ord xs = [ xs !! (read o ::Int) | o <- ord]
 
 -- Pass all rows into orderAll to be ordered
 selectAll :: [Order] -> [[String]] -> [[String]]
 selectAll ord xs = [selectOne ord x | x <- xs ]
+
+getOrder :: Exp -> [Order]
+getOrder (TakeFromWhere x y z) = x
+getOrder (TakeFrom x y) = x
 
 -- Take a list of lists and return each list as a string on a newline
 formatOutput :: [[String]] -> String
@@ -46,6 +56,7 @@ selectFrom ord xs ys = sort (selectAll ord (conjoin xs ys))
 
 getFileNames :: Exp -> [(String,[String])]
 getFileNames (TakeFromWhere x y z) = retrieveFile y
+getFileNames (TakeFrom x y) = retrieveFile y
 
 retrieveFile :: File -> [(String,[String])]
 retrieveFile (File name vars) = [(name,vars)]
@@ -55,7 +66,7 @@ readCsv :: String -> IO [[String]]
 readCsv file = do contents <- readFile file
                   let lineSep = lines contents
                   let lists = splitCommaList lineSep
-                  return lists 
+                  return lists
 
 splitCommaList :: [String] -> [[String]]
 splitCommaList strings = [splitCommaOnce s | s <- strings]
@@ -67,9 +78,10 @@ splitCommaOnce string = case dropWhile (==',') string of "" -> []
 
 readTables :: Exp -> IO [[String]]
 readTables (TakeFromWhere x y z) = readTable y
+readTables (TakeFrom x y) = readTable y
 
 -- Read in all the csvs in the query and store each as a separate list
---readTable :: File -> IO [[String]]
+readTable :: File -> IO [[String]]
 readTable (File name vars) = do file <- readCsv (name ++ ".csv")
                                 return (file)
 
@@ -81,6 +93,7 @@ readTable (Conjoin file1 file2) = do file1 <- readTable file1
 -- Create a list of associations between variable names and column indexes
 getAssocs :: Exp -> [VarMap]
 getAssocs (TakeFromWhere x y z) = zip (getVars y) [0..]
+getAssocs (TakeFrom x y) = zip (getVars y) [0..]
 
 -- Retrieve all the variables in the tables
 getVars :: File -> [String]
@@ -89,6 +102,8 @@ getVars (Conjoin file1 file2) = getVars file1 ++ getVars file2
 
 substituteExp :: Exp -> [VarMap] -> Exp
 substituteExp (TakeFromWhere x y z) assocs = TakeFromWhere (substituteStrings x assocs) (substituteFile y assocs) (substituteWheres z assocs)
+substituteExp (TakeFrom x y) assocs = TakeFrom (substituteStrings x assocs) (substituteFile y assocs)
+
 
 substituteFile :: File -> [VarMap] -> File
 substituteFile (File name vars) assocs = File name (substituteStrings vars assocs)
@@ -105,25 +120,14 @@ substituteWheres :: Where -> [VarMap] -> Where
 substituteWheres (Eq x) assocs = Eq (substituteString (fst x) assocs , substituteString (snd x) assocs)
 substituteWheres (And x y) assocs = And (substituteWheres x assocs) (substituteWheres y assocs)
 
---substituteString :: String -> [VarMap] -> String
---substituteString string assocs = 
-
--- Main
---main = do
---       readFirst <- readCsv "5a3.csv"
---       readSecond <- readCsv "5b3.csv"
---       let result = conjoin readFirst readSecond
---       let filtered = applyWhere result [(1,2)]
---       let final = sort $ selectAll [0,3] filtered       
---       print $ formatOutput final
 
 main = do
- progString <- readFile "program.txt" 
+ progString <- readFile "program.txt"
  let ast = parseCql $ alexScanTokens progString
  let assocs = getAssocs ast
  let subbedAst = substituteExp ast assocs
  conjoinedTable <- readTables subbedAst
- 
- print conjoinedTable
-
-
+ let wheres = createWherePairs subbedAst
+ let wheresApplied = applyWhere conjoinedTable wheres
+ let inOrder = selectAll (getOrder subbedAst) wheresApplied
+ print inOrder
